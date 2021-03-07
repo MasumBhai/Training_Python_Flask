576fa18f1099942bb368dd71fb9cd98d2dff3a90
Abdullah Al Masum <abdullahmasum6035@gmail.com>
2021-03-08 01:04:33 +0600

    Template inheritence with Boootstrap added successfully

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.register_blueprint(my_first_blueprint)
 @app.route('/')
 def home():
     content = "Masum Bhai is Learning Python Flask"
-    return render_template('index.html', value=content)
+    return render_template('index.html', valu=content)
 
 
 @app.route('/setcookie/', methods=['POST', 'GET'])
@@ -22,21 +22,24 @@ def set_cookie():
     response = make_response("cookie setted")
     response.set_cookie(key="MasumBhai's_flask_Cookie",
                         value="I will hack into your computer in a few days,let me learn hacking first",
-                        max_age=60*60*24
+                        max_age=60 * 60 * 24
                         )
-    return response
+    return render_template('Set_Cookies.html', value_for_set_cookie=response)
 
 
 @app.route('/getcookie/')
 def get_cookie():
     rsp = request.cookies.get("MasumBhai's_flask_Cookie")
-    return "The Cookie content is: "+rsp;
+    info = "The Cookie content is: " + rsp;
+    return render_template('Get_Cookie.html', value_for_cookie=info)
+
 
 @app.route('/delete_cookie/')
 def deleteCookie():
     res = make_response("Cookie has been Removed,refresh your browser cookie")
-    res.set_cookie(key="MasumBhai's_flask_Cookie",max_age=0)
-    return res
+    res.set_cookie(key="MasumBhai's_flask_Cookie", max_age=0)
+    return render_template('Deletion_Cookies.html', value_for_deleting_cookie=res)
+
 
 if __name__ == '__main__':
     # app.run(host="'0.0.0.0'", port=5000, debug=True)
